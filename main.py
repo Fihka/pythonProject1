38ea432f9a2dea53efa8b1e71f6c2cc79fc2eb91
Fihka <125051388+Fihka@users.noreply.github.com>
2023-02-10 19:41:34 +0300

    удалил цыикл

--- main.py
+++ main.py
@@ -3,17 +3,7 @@ password = input()
 def ask_password(password):
     count = 0
     correct_password = "12345"
-    while (True):
-        if (password == correct_password):
-            print("Доступ разрешён")
-            break
-        else:
-            print("Пароли не совпадают")
-            password = input()
-            count+=1
-            if count ==3:
-                print("net popitka")
-                break
+
 print(ask_password(password))
 jhvkghcvkj
 khvjgvjk.
